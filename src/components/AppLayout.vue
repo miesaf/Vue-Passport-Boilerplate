93e6c7ad9fbb83edd1c39fd591c420a2510f2da3
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2022-03-31 12:27:12 +0800

    Remove footer

--- src/components/AppLayout.vue
+++ src/components/AppLayout.vue
@@ -3,22 +3,19 @@
     <Header />
     <Nav />
     <router-view />
-    <Footer />
   </main>
 </template>
 
 <script>
 import Nav from '@/components/Nav'
 import Header from '@/components/Header'
-import Footer from '@/components/Footer'
 
 export default {
   name: "AppLayout",
 
   components: {
     Nav,
-    Header,
-    Footer
+    Header
   }
 }
 </script>
