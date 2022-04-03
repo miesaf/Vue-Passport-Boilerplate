35b374a674c0ed3af4c54f9e696f6151f157897c
Muhamad Amirul <19381137+miesaf@users.noreply.github.com>
2022-04-03 14:39:07 +0800

    Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,27 +1,49 @@
 <template>
-  <div>
-    <h1>Ini adalah Dashboard</h1>
-    <b-card
-      title="Card Title"
-      img-src="https://picsum.photos/600/300/?image=25"
-      img-alt="Image"
-      img-top
-      tag="article"
-      style="max-width: 20rem;"
-      class="mb-2"
-    >
-      <b-card-text>
-        Some quick example text to build on the card title and make up the bulk of the card's content.
-      </b-card-text>
+  <b-container>
+    <b-row align-h="center">
+      <b-col cols="12" md="8" class="py-3">
+        <h3 class="pb-2">Dashboard</h3>
+        
+        <b-alert
+          :show="alert.dismissCountDown"
+          dismissible
+          fade
+          :variant="alert.class"
+          @dismissed="alert.dismissCountDown=0"
+        >
+          <p>{{ alert.message }}</p>
+        </b-alert>
 
-      <b-button href="#" variant="primary">Go somewhere</b-button>
-    </b-card>
-  </div>
+        <b-card>
+          <b-card-text>Welcome to Vue + Laravel Passport Boilerplate, <b>{{ $store.state.user.authName }}</b>!</b-card-text>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 export default {
-  name: "SysDashboard"
+  name: "SysDashboard",
+
+  data() {
+    return {
+      alert: {
+        message: '',
+        class: '',
+        dismissCountDown: 0
+      }
+    }
+  },
+
+  created() {
+    var flashMessage = this.$session.flash.get("flash-message")
+    if(flashMessage) {
+      this.alert.message = flashMessage
+      this.alert.class = this.$session.flash.get("flash-class")
+      this.alert.dismissCountDown = 10
+    }
+  },
 }
 </script>
 
